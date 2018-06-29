dd2a3e6af8d493d485f4f289873b95d843655518
Alejo Castaño <drskantus@hotmail.com>
2018-06-29 22:50:56 +0200

    Appliying eslint

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
-import { Routes } from './app/components/routes/routes';
+import React, { Component } from "react";
+import { Routes } from "./app/components/routes/routes";
 
 class App extends React.Component {
   render() {
-    return (
-      <Routes />
-    );
+    return <Routes />;
   }
 }
 
--- App.specs.js
+++ App.specs.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import { shallow } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
-import renderer from 'react-test-renderer';
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+import renderer from "react-test-renderer";
 
-import App from './App';
+import App from "./App";
 
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   shallow(<App />);
 });
 
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
