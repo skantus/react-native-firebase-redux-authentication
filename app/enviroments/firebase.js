98949393e614495748a929e85c6e926428318367
Alejo Castaño <drskantus@hotmail.com>
2018-06-29 22:47:14 +0200

    Updating firebase enviroments

--- app/enviroments/firebase.js
+++ app/enviroments/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 const config = {
   apiKey: "AIzaSyBx0YuFCeqedFUGgpmdZOr7T7wGzy9N8Qo",
@@ -7,7 +7,7 @@ const config = {
   projectId: "pulso-3f393",
   storageBucket: "pulso-3f393.appspot.com",
   messagingSenderId: "677974717412"
-}
+};
 
 let instance = null;
 
@@ -23,19 +23,3 @@ class FirebaseService {
 
 const firebaseService = new FirebaseService().app;
 export default firebaseService;
-
-/*
-import * as firebase from 'firebase';
-
-const config = {
-  apiKey: "AIzaSyBx0YuFCeqedFUGgpmdZOr7T7wGzy9N8Qo",
-  authDomain: "pulso-3f393.firebaseapp.com",
-  databaseURL: "https://pulso-3f393.firebaseio.com",
-  projectId: "pulso-3f393",
-  storageBucket: "pulso-3f393.appspot.com",
-  messagingSenderId: "677974717412"
-};
-firebase.initializeApp(config);
-
-export default firebase;
-*/
