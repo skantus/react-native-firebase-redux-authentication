5cbbdda78215cb38a91fac8b0c89d8c0989cb57c
Alejo Castaño <drskantus@hotmail.com>
2018-06-29 21:55:51 +0200

    Improve SignupForm

--- app/components/auth/SignupForm/signupForm.js
+++ app/components/auth/SignupForm/signupForm.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react';
-import { View, Alert, Image } from 'react-native';
-import { BasicFormComponent } from '../BasicForm/basicForm';
-import { LoadingIndicator } from '../../loadingIndicator/loadingIndicator';
-import { styles } from '../BasicForm/styles';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { Actions } from 'react-native-router-flux';
+import React, { Component } from "react";
+import { View, Alert, Image } from "react-native";
+import { BasicFormComponent } from "../BasicForm/basicForm";
+import { LoadingIndicator } from "../../loadingIndicator/loadingIndicator";
+import { styles } from "../BasicForm/styles";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Actions } from "react-native-router-flux";
 
 export class SignupFormComponent extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
   componentDidUpdate(prevProps) {
-    if(this.props.registered) {
-      Actions.reset('home');
-    }
+    if (this.props.registered) Actions.reset("home");
   }
 
   render() {
     const { signup, loading } = this.props;
+    const { scrollView, imageBox, image, loginBox } = styles;
     return (
-      <KeyboardAwareScrollView style={styles.scrollView}>
-        <View style={styles.imageBox}>
-          <Image style={styles.image} source={{ uri: 'https://appjoy.org/wp-content/uploads/2016/06/firebase-authentication-logo1.png' }}/>
+      <KeyboardAwareScrollView style={scrollView}>
+        <View style={imageBox}>
+          <Image
+            style={image}
+            source={{
+              uri:
+                "https://appjoy.org/wp-content/uploads/2016/06/firebase-authentication-logo1.png"
+            }}
+          />
         </View>
-        <View style={styles.loginBox}>
-            {loading ? <LoadingIndicator color="#ffffff"
-                                         size="large"/> :
-              <BasicFormComponent buttonTitle={'signup'}
-                                  onButtonPress={signup} /> }
+        <View style={loginBox}>
+          {loading ? (
+            <LoadingIndicator color="#ffffff" size="large" />
+          ) : (
+            <BasicFormComponent buttonTitle={"signup"} onButtonPress={signup} />
+          )}
         </View>
       </KeyboardAwareScrollView>
     );
