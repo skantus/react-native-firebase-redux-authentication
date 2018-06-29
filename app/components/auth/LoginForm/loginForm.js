ba26e234e060013e191d2b21aa9a9c0d09080947
Alejo Castaño <drskantus@hotmail.com>
2018-06-29 21:53:09 +0200

    Improve LoginForm

--- app/components/auth/LoginForm/loginForm.js
+++ app/components/auth/LoginForm/loginForm.js
@@ -1,48 +1,49 @@
-import React, { Component } from 'react';
-import { View, Alert, Image, Button } from 'react-native';
-import { BasicFormComponent } from '../BasicForm/basicForm';
-import { LoadingIndicator } from '../../loadingIndicator/loadingIndicator';
-import { styles } from '../BasicForm/styles';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { Actions } from 'react-native-router-flux';
+import React, { Component } from "react";
+import { View, Alert, Image, Button } from "react-native";
+import { BasicFormComponent } from "../BasicForm/basicForm";
+import { LoadingIndicator } from "../../loadingIndicator/loadingIndicator";
+import { styles } from "../BasicForm/styles";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Actions } from "react-native-router-flux";
 
 export class LoginFormComponent extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     this.props.restore();
   }
 
   componentDidUpdate(prevProps) {
-    if (!prevProps.error && this.props.error) {
-      Alert.alert('error', this.props.error);
-    }
-    if(this.props.logged) {
-      Actions.reset('home');
-    }
+    const { error, logged } = this.props;
+
+    if (!prevProps.error && error) Alert.alert("error", error);
+
+    if (logged) Actions.reset("home");
   }
 
   render() {
     const { login, loading } = this.props;
+    const { scrollView, imageBox, image, loginBox } = styles;
     return (
-      <KeyboardAwareScrollView style={styles.scrollView}>
-        <View style={styles.imageBox}>
-          <Image style={styles.image} source={{ uri: 'https://lh3.googleusercontent.com/-whXBCDVxIto/Vz2Rsyz-UjI/AAAAAAAAiJc/UjvR-M2b9tY5SyKFkDY6Q_MbusEINRXkQ/w530-h530-n/Firebase_16-logo.png' }}/>
+      <KeyboardAwareScrollView style={scrollView}>
+        <View style={imageBox}>
+          <Image
+            style={image}
+            source={{
+              uri:
+                "https://lh3.googleusercontent.com/-whXBCDVxIto/Vz2Rsyz-UjI/AAAAAAAAiJc/UjvR-M2b9tY5SyKFkDY6Q_MbusEINRXkQ/w530-h530-n/Firebase_16-logo.png"
+            }}
+          />
         </View>
-        <View style={styles.loginBox}>
-          {loading ? <LoadingIndicator color="#ffffff"
-                                       size="large"/> :
-            <BasicFormComponent buttonTitle={'login'}
-                                onButtonPress={login} /> }
+        <View style={loginBox}>
+          {loading ? (
+            <LoadingIndicator color="#ffffff" size="large" />
+          ) : (
+            <BasicFormComponent buttonTitle={"login"} onButtonPress={login} />
+          )}
         </View>
         <View>
-        {loading ? null :
-          <Button onPress={Actions.signup}
-                  title="Signup"
-                  color="white"></Button> }
+          {loading ? null : (
+            <Button onPress={Actions.signup} title="Signup" color="white" />
+          )}
         </View>
       </KeyboardAwareScrollView>
     );
