e4b1a4068e1b9b7acfa0fae43e6ed7a13b0eb9b9
Alejo Castaño <drskantus@hotmail.com>
2018-09-10 16:06:27 +0200

    Updating counterOutput.js

--- app/components/counter/counterOutput.js
+++ app/components/counter/counterOutput.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-import { H2 } from "native-base";
 import { styles } from "./styles";
 
-export class CounterOutput extends React.Component {
+export class CounterOutput extends Component {
   render() {
     const { value } = this.props;
     return (
       <View style={styles.marginBox}>
-        <H2>{String(value)}</H2>
+        <Text>{String(value)}</Text>
       </View>
     );
   }
